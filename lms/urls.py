d2d972c4a701cf24fe2982ca2f9202a6c7ab9732
Robert Raposa <rraposa@edx.org>
2016-10-06 15:26:15 -0400

    Move track.views to new urls.py

--- lms/urls.py
+++ lms/urls.py
@@ -32,9 +32,11 @@ urlpatterns = (
     url(r'^login_ajax/(?P<error>[^/]*)$', 'student.views.login_user'),
 
     url(r'^email_confirm/(?P<key>[^/]*)$', 'student.views.confirm_email_change'),
-    url(r'^event$', 'track.views.user_track'),
+
+    # Event tracking endpoints
+    url(r'', include('track.urls')),
+
     url(r'^performance$', 'openedx.core.djangoapps.performance.views.performance_log'),
-    url(r'^segmentio/event$', 'track.views.segmentio.segmentio_event'),
 
     # TODO: Is this used anymore? What is STATIC_GRAB?
     url(r'^t/(?P<template>[^/]*)$', 'static_template_view.views.index'),
