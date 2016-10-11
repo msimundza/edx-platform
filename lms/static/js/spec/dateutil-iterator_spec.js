9f2de5d02ccccbf5ab34e2c914d2794a23d0b3fa
Greg Martin <greg@edx.org>
2016-10-11 16:02:47 -0400

    spec test

--- lms/static/js/spec/dateutil-iterator_spec.js
+++ lms/static/js/spec/dateutil-iterator_spec.js
@@ -1,4 +1,4 @@
-define(['../date-util-iterator.js'], function(DateUtilIterator) {
+define(['../dateutil-iterator.js'], function(DateUtilIterator) {
     'use strict';
 
     describe('DateUtilIterator', function() {
@@ -15,46 +15,60 @@ define(['../date-util-iterator.js'], function(DateUtilIterator) {
             });
         });
 
-        describe('localizedTime', function() {
-            var context;
-            it('returns an empty string if provided no time', function() {
-                context = {}
-                expect(DateUtilIterator.localizedTime(context)).toEqual('');
-            });
-            it('returns a timezone formatted string', function() {
-                var TestLangs = {
-                    en: 'Oct 14, 2016 08:00 UTC',
-                    ru: '14 окт. 2016 г. 08:00 UTC',
-                    ar: '١٤ تشرين الأول أكتوبر ٢٠١٦ ٠٨:٠٠ UTC'
-                };
-                Object.keys(TestLangs).forEach(function(key) {
-                    console.log(key)
-                    var context = {
-                        datetime: '2016-10-14 08:00:00+00:00',
-                        timezone: 'UTC',
-                        language: key
-                    };
-                    console.log(context)
-                    expect(DateUtilIterator.localizedTime(context)).toEqual(TestLangs[key]);
-                });
-            });
-        });
-        describe('transform', function() {
-           console.log('WORK HERE')
-        });
-        // transform
-        // localizedTime: localizedTime,
-        // stringHandler: stringHandler
-
-        // describe('iterator', function() {
-        // it('returns an empty string if provided no time', function() {
-        //         expect(DateUtils.stringToMoment()).toEqual(undefined);
+        // describe('localizedTime', function() {
+        //     var context;
+        //     it('returns an empty string if provided no time', function() {
+        //         context = {}
+        //         expect(DateUtilIterator.localizedTime(context)).toEqual('');
         //     });
-        //     it('converts a string to a moment.js object', function() {
-        //         expect(
-        //             DateUtils.stringToMoment('2016-10-14 08:00:00+00:00').format('ll')
-        //         ).toEqual('Oct 14, 2016');
+        //     it('returns a timezone formatted string', function() {
+        //         var TestLangs = {
+        //             en: 'Oct 14, 2016 08:00 UTC',
+        //             ru: '14 окт. 2016 г. 08:00 UTC',
+        //             ar: '١٤ تشرين الأول أكتوبر ٢٠١٦ ٠٨:٠٠ UTC',
+        //             fr: 'FARTFART'
+        //         };
+        //         Object.keys(TestLangs).forEach(function(key) {
+        //             console.log(key);
+        //             var context = {
+        //                 datetime: '2016-10-14 08:00:00+00:00',
+        //                 timezone: 'UTC',
+        //                 language: String(key)
+        //             };
+        //             console.log(context);
+        //             expect(DateUtilIterator.localizedTime(context)).toEqual(TestLangs[key]);
+        //         });
         //     });
         // });
+        describe('transform', function() {
+            // describe(function(){
+            var form;
+
+            // beforeEach(function(){});
+            it('localizes some times', function() {
+                /* we have to generate a fake span and then test the resultant texts */
+                var iterationKey = '.localized-datetime';
+                var TestLangs = {
+                    en: 'due Oct 14, 2016 08:00 UTC',
+                    ru: '14 окт. 2016 г. 08:00 UTC',
+                    ar: '١٤ تشرين الأول أكتوبر ٢٠١٦ ٠٨:٠٠ UTC',
+                    fr: 'FARTFART'
+                };
+                var form = $('<span class="subtitle-name localized-datetime" data-timezone="UTC" data-datetime="2016-10-14 08:00:00+00:00" data-string="due"></span>');
+                // beforeEach(function() {
+                // });
+                Object.keys(TestLangs).forEach(function(key) {
+                    form.attr('lang', key);
+                    $(document.body).append(form);
+
+                    DateUtilIterator.transform(iterationKey);
+                    expect(form.text()).toEqual(TestLangs[key]);
+
+                    form.remove();
+                    // form = null;
+                });
+                form = null;
+            });
+        });
     });
 });
